0ab08ee88a3051c99305975096d8d7b2719bcde4
Roshan Raj <roshan139154@gmail.com>
2022-01-04 15:08:22 +0530

    API which scrapes the content form the website

--- routes/scrapeRoute.js
+++ routes/scrapeRoute.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var app = express.Router();
 
-//==============================================================================
-app.get('/', (req, res) => {
+const { scrapeChessData } = require('../utils/webScrapeUtil')
 
-    res.type('application/json');
-    res.status(200).send(JSON.stringify("message", null, 2));
+const exceptionMessage = "Something went wrong, Please try again!";
+
+//==============================================================================
+/**
+ * @description List all of the data from `https://www.chessgames.com/chessecohelp.html` in a JSON format.
+ * 
+ * @author Roshan Raj
+ * @since 04-01-2022
+ */
+app.get('/', async (req, res) => {
+    try {
+        const scrapeChessDataResult = await scrapeChessData();
+        res.type('application/json');
+        res.status(200).send(JSON.stringify(scrapeChessDataResult, null, 2));
+    } catch (exception) {
+        res.status(500).send(JSON.stringify(exceptionMessage, null, 2));
+    }
+});
+
+//==============================================================================
+
+app.get('/:moveCode', async (req, res) => {
+    try {
+        const moveCode = (req.params.moveCode).toUpperCase();
+        const scrapeChessDataResult = await scrapeChessData();
+        res.type('application/json');
+        res.status(200).send(JSON.stringify(scrapeChessDataResult, null, 2));
+    } catch (exception) {
+        res.status(500).send(JSON.stringify(exceptionMessage, null, 2));
+    }
 });
 
 //==============================================================================
