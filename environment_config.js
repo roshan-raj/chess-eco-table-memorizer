e6a017276a6b5d30234259aa84065d8a0537124a
Roshan Raj <roshan139154@gmail.com>
2022-01-04 15:45:20 +0530

    have single env

--- environment_config.js
+++ environment_config.js
@@ -1,17 +1,7 @@
 // config.js
 const dotenv = require('dotenv');
 
-//get the NODE_ENV from the arguments set in the package.json file
-const NODE_ENV = process.argv[2] || process.env.NODE_ENV;
-process.env.NODE_ENV = NODE_ENV;
-
-let result;
-
-if (NODE_ENV == 'development')
-    result = dotenv.config({ path: './.env.development' });
-    
-if (NODE_ENV == 'production')
-    result = dotenv.config({ path: './.env.production' });
+let result = dotenv.config({ path: './.env' });
 
 if (result.error) {
     throw result.error;
