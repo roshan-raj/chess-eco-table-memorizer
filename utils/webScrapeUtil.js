48504e2c440dab17fc7ac4e024a2e1693c1546ff
Roshan Raj <roshan139154@gmail.com>
2022-01-04 15:22:54 +0530

    Refactored the codebase

--- utils/webScrapeUtil.js
+++ utils/webScrapeUtil.js
@@ -4,49 +4,71 @@ const cheerio = require("cheerio");
 //==============================================================================
 
 const scrapeChessData = async () => {
-    try {
-        const result = await axios.get("https://www.chessgames.com/chessecohelp.html");
-        const $ = cheerio.load(result.data);
-        const scrapedData = [];
-        const moveCode = [];
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await axios.get("https://www.chessgames.com/chessecohelp.html");
+            const $ = cheerio.load(result.data);
+            const scrapedData = [];
+            const moveCode = [];
 
-        $("body > font > p > table > tbody > tr > td").each((index, element) => {
-            // TODO: Search a better filter, this will work for now.
-            if ($(element).text().length === 3)
-                moveCode.push($(element).find("font").text())
-        });
+            $("body > font > p > table > tbody > tr > td").each((index, element) => {
+                // TODO: Search a better filter, this will work for now.
+                if ($(element).text().length === 3)
+                    moveCode.push($(element).find("font").text())
+            });
 
-        $("body > font > p > table > tbody > tr > td > font").each((index, element) => {
+            $("body > font > p > table > tbody > tr > td > font").each((index, element) => {
 
-            let moveName = $(element).find("B").text()
-            let move = $(element).find("font").text()
+                let moveName = $(element).find("B").text()
+                let move = $(element).find("font").text()
 
-            let obj = {};
-            if (moveName.length > 0)
-                obj.moveName = moveName
+                let obj = {};
+                if (moveName.length > 0)
+                    obj.moveName = moveName
 
-            if (move.length > 0)
-                obj.move = move
+                if (move.length > 0)
+                    obj.move = move
 
-            if (obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype) {
+                if (obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype) {
 
-            } else
-                scrapedData.push(obj);
-        });
+                } else
+                    scrapedData.push(obj);
+            });
 
-        for (let index = 0; index < moveCode.length; index++) {
-            const element = moveCode[index];
-            scrapedData[index].moveCode = element
+            for (let index = 0; index < moveCode.length; index++) {
+                const element = moveCode[index];
+                scrapedData[index].moveCode = element
+            }
+
+            return resolve(scrapedData);
+        } catch (exception) {
+            console.log(exception)
+            return reject(exception)
         }
+    })
+}
 
-        return scrapedData;
-    } catch (exception) {
-        console.log(exception)
-    }
+//==============================================================================
+
+const getMoveDetails = async (moveCode, chessDataArray) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            for (let index = 0; index < chessDataArray.length; index++) {
+                const element = chessDataArray[index];
+                if (moveCode === element.moveCode) {
+                    return resolve(element);
+                }
+            }
+            return resolve(false);
+        } catch (exception) {
+            return reject(exception)
+        }
+    })
 }
 
 //==============================================================================
 
 module.exports = {
-    scrapeChessData
+    scrapeChessData,
+    getMoveDetails
 }
